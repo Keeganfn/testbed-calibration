5940f465ab75c10952d649c42d0688337f0e0929
Garrett Scott <Garret-t@users.noreply.github.com>
2022-02-13 22:52:02 +0000

    Updated to latest GUI revision with template code

--- calibration/src/rqt_calibration_gui/calibrationGUI.py
+++ calibration/src/rqt_calibration_gui/calibrationGUI.py
@@ -34,26 +34,41 @@ class CalibrationGUI(Plugin):
 
     # Setup button event listeners
     self._widget.importButton.clicked[bool].connect(self.handle_import_clicked)
-    self._widget.calibrateInternalCameraButton.clicked[bool].connect(self.handle_internal_camera_calibration_clicked)
+    self._widget.pictureButton.clicked[bool].connect(self.handle_take_picture_clicked)
     self._widget.cameraPoseButton.clicked[bool].connect(self.handle_calibrate_camera_pose_clicked)
-    self._widget.recordButton.clicked[bool].connect(self.handle_record_touch_pt_clicked)
+    self._widget.upperLeftButton.clicked[bool].connect(self.handle_upper_left_touchpt_clicked)
+    self._widget.upperRightButton.clicked[bool].connect(self.handle_upper_right_touchpt_clicked)
+    self._widget.lowerLeftButton.clicked[bool].connect(self.handle_lower_left_touchpt_clicked)
+    self._widget.lowerRightButton.clicked[bool].connect(self.handle_lower_right_touchpt_clicked)
+    self._widget.saveCamSettingsButton.clicked[bool].connect(self.handle_save_camera_settings_clicked)
+    self._widget.viewTButton.clicked[bool].connect(self.handle_view_top_clicked)
+    self._widget.viewSButton.clicked[bool].connect(self.handle_view_side_clicked)
+    self._widget.saveAsButton.clicked[bool].connect(self.handle_save_as_clicked)
+    self._widget.startRVizButton.clicked[bool].connect(self.handle_start_RViz_clicked)
 
     # Setup combo box event listeners
     self._widget.testBedComboBox.currentIndexChanged.connect(self.handle_testbed_change)
     self._widget.armComboBox.currentIndexChanged.connect(self.handle_arm_change)
-    self._widget.cameraComboBox.currentIndexChanged.connect(self.handle_camera_change)
+    self._widget.cameraComboBoxT.currentIndexChanged.connect(self.handle_camera_T_change)
+    self._widget.cameraComboBoxS.currentIndexChanged.connect(self.handle_camera_S_change)
+
+    # Get zdist value manually 
+    #self._widget.zDistSpinBox.value()
+
+    #Inital guess values
+    #self._widget.xSpinBox.value()
+    #self._widget.ySpinBox.value()
+    #self._widget.zSpinBox.value()
+    
   
   def handle_import_clicked(self):
     print("importing internal camera calibration settings")
 
-  def handle_internal_camera_calibration_clicked(self):
-    print("calibrating internal camera")
+  def handle_take_picture_clicked(self):
+    print("taking picture")
 
   def handle_calibrate_camera_pose_clicked(self):
     print("calibrating camera pose")
-
-  def handle_record_touch_pt_clicked(self):
-    print("recording touchpoint")
 
   def handle_testbed_change(self, i):
       print("Testbed index is:", i, "Value:", self._widget.testBedComboBox.currentText())
@@ -61,6 +76,35 @@ class CalibrationGUI(Plugin):
   def handle_arm_change(self, i):
       print("Arm index is:", i, "Value:", self._widget.armComboBox.currentText())
 
-  def handle_camera_change(self, i):
-      print("Camera index is:", i, "Value:", self._widget.cameraComboBox.currentText())
+  def handle_camera_T_change(self, i):
+      print("Camera index is:", i, "Value:", self._widget.cameraComboBoxT.currentText())
 
+  def handle_camera_S_change(self, i):
+      print("Camera index is:", i, "Value:", self._widget.cameraComboBoxS.currentText())
+
+  def handle_upper_left_touchpt_clicked(self):
+      print("getting upper left touchpt")
+
+  def handle_upper_right_touchpt_clicked(self):
+      print("getting upper right touchpt")
+
+  def handle_lower_left_touchpt_clicked(self):
+      print("getting lower left touchpt")
+
+  def handle_lower_right_touchpt_clicked(self):
+      print("getting lower right touchpt")
+  
+  def handle_save_camera_settings_clicked(self):
+      print("saving camera settings")
+  
+  def handle_view_top_clicked(self):
+      print("viewing top camera")
+  
+  def handle_view_side_clicked(self):
+      print("viewing side camera")
+  
+  def handle_save_as_clicked(self):
+      print("saving settings")
+  
+  def handle_start_RViz_clicked(self):
+      print("starting RViz")
