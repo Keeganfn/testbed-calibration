f9de9403b09495476f296c530b7c7e5818cefd09
Cyrus Swihart <swihartc@oregonstate.edu>
2022-03-03 02:34:53 +0000

    Stuff

--- calibration/src/arm_calibration.py
+++ calibration/src/arm_calibration.py
@@ -28,38 +28,102 @@ class ArmCalibration:
     # *******************************************************************************************
     def calibrate_arm(self, tp_data):
         
-        trans_guess = np.array([
-        [1, 0, 0, 0],
-        [0, 1, 0, .53],
+        # Estimated Transformation Matrix: Origin -> Robot arm Base
+        estOriginToBase = np.array([
+        [1, 0, 0, -.597],
+        [0, 1, 0, 0],
         [0, 0, 1, .035],
         [0, 0, 0, 1]])
 
-        print(tp_data)
-
-        return trans_guess
-
-
+        arr_translations = self.__getTranslations(estOriginToBase, tp_data)
         
+        newMatrix = self.__updateTransMatrix(arr_translations, estOriginToBase)
         
+        return newMatrix
+
+    # Returns a 1x4 vector containing the XYZ coordinates of the end effector in the world frame.
+    def __getEndEffInWorld(self, originToBase, endEffToBase):
+
+        test = np.matmul(originToBase, endEffToBase)
+        endEffInWorld = np.matmul(test, np.transpose([0, 0, 0, 1]))
+
+        return endEffInWorld
+    
+    # Returns 4 arrays containing the XYZ coordinates of the end effector positions in the world frame.
+    def __getTranslations(self, originToBase, tp_data):
+        
+        arr_endEffInWorld = []
+
+        for i in range(0, len(tp_data)):
+            endEffCoords = self.__getEndEffInWorld(originToBase, tp_data[i])
+            arr_endEffInWorld.append(endEffCoords)
+
+        return arr_endEffInWorld
 
 
+    # Calibrates the XYZ location of the robot arm base in the world frame.
+    def __calibrateTranslations(self, arr_translations):
+
+        x = 0
+        y = 0
+        z = 0
+        
+        arr_len = len(arr_translations)
+
+        for i in range(arr_len):
+            x += arr_translations[i][0]
+            y += arr_translations[i][1]
+            z += arr_translations[i][2]
+
+        dx = x / arr_len
+        dy = y / arr_len
+        dz = z / arr_len
+
+        return (dx, dy, dz)
+
+    # Calibrates the rotation portion of the transformation matrix.
+    def __calibrateRotations(self, arr_translations):
+
+        print(arr_translations[0][:-1])
+        v_12 = arr_translations[1][:-1] - arr_translations[2][:-1]
+        v_43 = arr_translations[0][:-1] - arr_translations[3][:-1]
+        x_vec = (v_12 + v_43) / 2
+
+        v_14 = arr_translations[0][:-1] - arr_translations[1][:-1]
+        v_23 = arr_translations[3][:-1] - arr_translations[2][:-1]
+        y_vec = (v_14 + v_23) / 2
+
+        yp_vec = y_vec - np.dot(x_vec, y_vec) * x_vec
 
 
-    '''
-    def joint_angle_subscriber(self, angles):
-        #This is where we will subscribe to the arm joint angles in the future
-        pass
+        z_vec = np.cross(x_vec, yp_vec)
 
-    def record_touchpoint_server(self, msg):
-        #This is where the client in the GUI will tell us to record our joint angles and point (4 in total)
-        pass
+        rotationMatrix = np.identity(4)
 
-    def arm_transormation_martix_server(self, msg):
-        #This is where the client in the GUI will query when it wants the transformation matrix from arm to table
-        pass
+        rotationMatrix[0][:-1] = x_vec / np.linalg.norm(x_vec)
+        rotationMatrix[1][:-1] = yp_vec / np.linalg.norm(yp_vec)
+        rotationMatrix[2][:-1] = z_vec / np.linalg.norm(z_vec)
 
-    '''
+
+        return rotationMatrix
+
+
+    # Calibrates the original guess transformation matrix and returns the updated matrix.
+    def __updateTransMatrix(self, arr_translations, oldMatrix):
+
+        (dx, dy, dz) = self.__calibrateTranslations(arr_translations)
+
+        newMatrix = self.__calibrateRotations(arr_translations)
+
+        newMatrix[0][-1] = oldMatrix[0][-1] - dx
+        newMatrix[1][-1] = oldMatrix[1][-1] - dy
+        newMatrix[2][-1] = oldMatrix[2][-1] - dz
+
+        return newMatrix
+
+        #print(dx, dy, dz)
+
 
 if __name__ == "__main__":
 
-    ArmCalibration().calibrate_arm(1)
+    print("Run me via the test script, arm_calibration_test.py")
